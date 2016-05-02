ff091f706d20cbfde3c841394d58b5761c635387
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2016-05-02 17:57:44 +0100

    Factor testing client into a fixture

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,13 +1,18 @@
+import pytest
+
 from atmdb import TMDbClient
 
+TOKEN = 'some_api_token'
 
-def test_client_instantiation():
-    token = 'some_api_token'
-    client = TMDbClient(api_token=token)
-    assert client.api_token == token
+@pytest.fixture
+def client():
+    return TMDbClient(api_token=TOKEN)
 
 
-def test_client_auth():
-    client = TMDbClient(api_token='something')
-    expected = 'https://api.themoviedb.org/3/endpoint?api_key=something'
+def test_client_instantiation(client):
+    assert client.api_token == TOKEN
+
+
+def test_client_auth(client):
+    expected = 'https://api.themoviedb.org/3/endpoint?api_key={}'.format(TOKEN)
     assert client.url_builder('endpoint') == expected
